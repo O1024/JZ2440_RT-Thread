c7ab69cb336c2ec6434750bc1cd4b4900ac8c8d3
chenw <wenchen.mails@gmail.com>
2025-10-10 17:48:36 +0800

    feat(build): delete keil conf

---
+++ setenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# BSP板级代码目录
+export BSP_ROOT="$PWD/bsp/mini2440"
+
+# RT代码路径
+export RTT_ROOT="$PWD"
+
+# 交叉编译工具链路径
+export RTT_EXEC_PATH="/opt/gcc-arm-none-eabi-10.3-2021.10/bin"
+
+# 交叉编译工具链前缀
+export RTT_CC_PREFIX="arm-none-eabi-"
